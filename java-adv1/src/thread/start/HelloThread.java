a97153b392e413ab7c2f91666ce894009abbde1d
Jihun Park <jihunpark.tech@gmail.com>
2024-07-29 10:55:34 +0900

    [java adv1] 스레드 시작1

--- java-adv1/src/thread/start/HelloThread.java
+++ java-adv1/src/thread/start/HelloThread.java
@@ -1,6 +1,9 @@
 package thread.start;
 
 public class HelloThread extends Thread {
+    /**
+     * Thread 클래스를 상속 받아서 스레드를 생성
+     */
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + ": run()");
