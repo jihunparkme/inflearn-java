f813ceadf54d9dcbe95f8e38619843056042fd43
Jihun Park <jihunpark.tech@gmail.com>
2024-08-02 10:05:49 +0900

    [java adv1] join - sleep 사용

---
+++ java-adv1/src/thread/control/join/JoinMainV2.java
@@ -0,0 +1,60 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinMainV2 {
+    public static void main(String[] args) {
+        log("Start");
+
+        SumTask task1 = new SumTask(1, 50);
+        SumTask task2 = new SumTask(51, 100);
+
+        Thread thread1 = new Thread(task1, "thread-1");
+        Thread thread2 = new Thread(task2, "thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        // 정확한 타이밍을 맞춰서 기다리기 어려움
+        log("main 스레드 sleep()");
+        sleep(3000);
+        log("main 스레드 깨어남");
+
+        log("task1.result = " + task1.result);
+        log("task2.result = " + task2.result);
+
+        int sumAll = task1.result + task2.result;
+        log("task1 + task2 = " + sumAll);
+
+        log("End");
+    }
+
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        /**
+         * this 는 호출된 인스턴스 메서드가 소속된 객체를 가리키는 참조이며, 이것이 스택 프레임 내부에 저장
+         */
+        @Override
+        public void run() {
+            log("작업 시작");
+
+            sleep(2000);
+            int sum = 0;
+            for (int i = this.startValue; i <= this.endValue; i++) {
+                sum += i;
+            }
+            this.result = sum;
+
+            log("작업 완료 result = " + this.result);
+        }
+    }
+}
