2c21b02a50957803c6a4fb4d5415f2560045203d
Jihun Park <jihunpark.tech@gmail.com>
2024-08-02 10:13:09 +0900

    [java adv1] join - join 사용

--- java-adv1/src/thread/control/join/JoinMainV3.java
+++ java-adv1/src/thread/control/join/JoinMainV3.java
@@ -17,6 +17,12 @@ public class JoinMainV3 {
         thread1.start();
         thread2.start();
 
+        // 스레드가 종료될 때 까지 대기
+        log("join() - main 스레드가 thread1, thread2 종료까지 대기");
+        thread1.join();
+        thread2.join();
+        log("main 스레드 대기 완료");
+
         log("task1.result = " + task1.result);
         log("task2.result = " + task2.result);
 
