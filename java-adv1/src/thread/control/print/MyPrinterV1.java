ad1a54bf021ca860b9e1fca9c3e81b6b0d59bb20
Jihun Park <jihunpark.tech@gmail.com>
2024-08-06 08:25:35 +0900

    [java adv1] 프린터 예제3 - 인터럽트 코드 개선

--- java-adv1/src/thread/control/print/MyPrinterV1.java
+++ java-adv1/src/thread/control/print/MyPrinterV1.java
@@ -18,7 +18,6 @@ public class MyPrinterV1 {
             log("프린터할 문서를 입력하세요. 종료 (q): ");
             String input = userInput.nextLine();
             if (input.equals("q")) {
-                printer.work = false;
                 printerThread.interrupt();
                 break;
             }
@@ -27,21 +26,16 @@ public class MyPrinterV1 {
     }
 
     /**
-     * volatile
-     * - 여러 스레드가 동시에 접근하는 변수에는 volatile 키워드를 사용
-     * - main 스레드, printer 스레드 둘다 work 변수에 동시에 접근 가능
-     *
      * ConcurrentLinkedQueue
      * - 여러 스레드가 동시에 접근하는 경우, 컬렉션 프레임워크가 제공하는 일반적인 자료구조를 사용하면 위험
      * - 동시성을 지원하는 동시성 컬렉션을 사용
      */
     static class Printer implements Runnable {
-        volatile boolean work = true;
         Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
 
         @Override
         public void run() {
-            while (work) {
+            while (!Thread.interrupted()) {
                 if (jobQueue.isEmpty()) {
                     continue;
                 }
