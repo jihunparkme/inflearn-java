59710931eb9b510d05569e802955903f9605dce1
Jihun Park <jihunpark.tech@gmail.com>
2024-08-06 08:17:35 +0900

    [java adv1] 프린터 예제1 - 시작

---
+++ java-adv1/src/thread/control/print/MyPrinterV1.java
@@ -0,0 +1,61 @@
+package thread.control.print;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class MyPrinterV1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printer.work = false;
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    /**
+     * volatile
+     * - 여러 스레드가 동시에 접근하는 변수에는 volatile 키워드를 사용
+     * - main 스레드, printer 스레드 둘다 work 변수에 동시에 접근 가능
+     *
+     * ConcurrentLinkedQueue
+     * - 여러 스레드가 동시에 접근하는 경우, 컬렉션 프레임워크가 제공하는 일반적인 자료구조를 사용하면 위험
+     * - 동시성을 지원하는 동시성 컬렉션을 사용
+     */
+    static class Printer implements Runnable {
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while (work) {
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                String job = jobQueue.poll();
+                log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                sleep(3000);
+                log("출력 완료: " + job);
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
