6132aa073db6742081a6a056c25bce5c153304e6
Jihun Park <jihunpark.tech@gmail.com>
2024-08-10 18:54:48 +0900

    [java adv1] volatile, 메모리 가시성3

--- java-adv1/src/thread/volatile1/VolatileFlagMain.java
+++ java-adv1/src/thread/volatile1/VolatileFlagMain.java
@@ -24,9 +24,26 @@ public class VolatileFlagMain {
          * volatile 미사용 시
          * 예상과 다르게 task 가 종료되지 않고, 자바 프로그램도 계속 실행
          * work 스레드가 while 문에서 빠져나오지 못하고 있는 상황
+         *
+         * [     main] runFlag = true
+         * [     work] task 시작
+         * [     main] runFlag 를 false 로 변경 시도
+         * [     main] runFlag = false
+         * [     main] main 종료
          */
-        boolean runFlag = true;
-        //volatile boolean runFlag = true;
+        // boolean runFlag = true;
+
+        /**
+         * 캐시 메모리를 사용하지 않고, 값을 읽거나 쓸 때 항상 메인 메모리에 직접 접근
+         *
+         * [     main] runFlag = true
+         * [     work] task 시작
+         * [     main] runFlag 를 false 로 변경 시도
+         * [     work] task 종료 (*)
+         * [     main] runFlag = false
+         * [     main] main 종료
+         */
+        volatile boolean runFlag = true;
 
         @Override
         public void run() {
