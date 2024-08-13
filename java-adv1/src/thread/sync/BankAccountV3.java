68a127db284e3aa2c456a5af26fe4f10d7c3e86f
Jihun Park <jihunpark.tech@gmail.com>
2024-08-13 09:36:07 +0900

    [java adv1] synchronized 코드 블럭

--- java-adv1/src/thread/sync/BankAccountV3.java
+++ java-adv1/src/thread/sync/BankAccountV3.java
@@ -12,19 +12,21 @@ public class BankAccountV3 implements BankAccount {
     }
 
     @Override
-    public synchronized boolean withdraw(int amount) {
+    public boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
-        log("[검증 시작] 출금액: " + amount + ", 잔액: " + this.balance);
-        if (this.balance < amount) {
-            log("[검증 실패] 출금액: " + amount + ", 잔액: " + this.balance);
-            return false;
-        }
+        synchronized (this) { // -> 내(this) 인스턴스에서 락을 획득
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + this.balance);
+            if (this.balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + this.balance);
+                return false;
+            }
 
-        log("[검증 완료] 출금액: " + amount + ", 잔액: " + this.balance);
-        sleep(1000); // 출금에 걸리는 시간으로 가정
-        this.balance = this.balance - amount;
-        log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + this.balance);
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + this.balance);
+            sleep(1000); // 출금에 걸리는 시간으로 가정
+            this.balance = this.balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + this.balance);
+        }
 
         log("거래 종료");
         return true;
