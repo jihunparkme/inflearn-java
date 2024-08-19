f25201f8cf7e51fbc5962c463423566e99a3ce2d
Jihun Park <jihunpark.tech@gmail.com>
2024-08-20 08:07:22 +0900

    [java adv1] ReentrantLock 활용 준비

---
+++ java-adv1/src/thread/sync/BankAccountV4.java
@@ -0,0 +1,39 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+
+        synchronized (this) { // -> 내(this) 인스턴스에서 락을 획득
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + this.balance);
+            if (this.balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + this.balance);
+                return false;
+            }
+
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + this.balance);
+            sleep(1000); // 출금에 걸리는 시간으로 가정
+            this.balance = this.balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + this.balance);
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return this.balance;
+    }
+}
