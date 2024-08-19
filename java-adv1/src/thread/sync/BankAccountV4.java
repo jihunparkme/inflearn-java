dd0ee63f0a27d8688069701ea92a76eb38e4b1b8
Jihun Park <jihunpark.tech@gmail.com>
2024-08-20 08:21:11 +0900

    [java adv1] ReentrantLock 활용

--- java-adv1/src/thread/sync/BankAccountV4.java
+++ java-adv1/src/thread/sync/BankAccountV4.java
@@ -1,11 +1,16 @@
 package thread.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 public class BankAccountV4 implements BankAccount {
 
     private int balance;
+
+    private final Lock lock = new ReentrantLock();
 
     public BankAccountV4(int initialBalance) {
         this.balance = initialBalance;
@@ -15,7 +20,12 @@ public class BankAccountV4 implements BankAccount {
     public boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
-        synchronized (this) { // -> 내(this) 인스턴스에서 락을 획득
+        /**
+         * ReentrantLock 을 활용한 락
+         * Lock 인터페이스와 ReentrantLock 이 제공하는 기능
+         */
+        lock.lock();
+        try {
             log("[검증 시작] 출금액: " + amount + ", 잔액: " + this.balance);
             if (this.balance < amount) {
                 log("[검증 실패] 출금액: " + amount + ", 잔액: " + this.balance);
@@ -26,14 +36,24 @@ public class BankAccountV4 implements BankAccount {
             sleep(1000); // 출금에 걸리는 시간으로 가정
             this.balance = this.balance - amount;
             log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + this.balance);
+        } finally {
+            /**
+             * ReentrantLock 을 활용한 락 해제
+             * - 락을 반납하지 않을 경우 대기하는 스레드가 락을 얻지 못함
+             */
+            lock.unlock();
         }
-
         log("거래 종료");
         return true;
     }
 
     @Override
-    public synchronized int getBalance() {
-        return this.balance;
+    public int getBalance() {
+        lock.lock();
+        try {
+            return this.balance;
+        } finally {
+            lock.unlock();
+        }
     }
 }
