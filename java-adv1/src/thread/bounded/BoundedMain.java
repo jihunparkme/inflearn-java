58da5718e78d1e76c7820caa73a9bb44295513aa
Jihun Park <jihunpark.tech@gmail.com>
2024-08-21 10:09:46 +0900

    [java adv1] 생산자 소비자 문제 example 02

--- java-adv1/src/thread/bounded/BoundedMain.java
+++ java-adv1/src/thread/bounded/BoundedMain.java
@@ -10,12 +10,11 @@ public class BoundedMain {
 
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택!
-
         /**
-         * == [생산자 먼저 실행] 시작, BoundedQueueV1 ==
+         * == [생산자 먼저 실행] 시작, BoundedQueueV2 ==
          *
          * 생산자 시작
          * [생산 시도] data1 -> []
@@ -23,68 +22,66 @@ public class BoundedMain {
          * [생산 시도] data2 -> [data1]
          * [생산 완료] data2 -> [data1, data2]
          * [생산 시도] data3 -> [data1, data2]
-         * [put] 큐가 가득 참, 버림: data3
-         * [생산 완료] data3 -> [data1, data2]
+         * [put] 큐가 가득 참, 생산자 대기
          *
          * 현재 상태 출력, 큐 데이터: [data1, data2]
          * producer1: TERMINATED
          * producer2: TERMINATED
-         * producer3: TERMINATED
+         * producer3: TIMED_WAITING
          *
          * 소비자 시작
          * [소비 시도] ? <- [data1, data2]
-         * [소비 완료] data1 <- [data2]
-         * [소비 시도] ? <- [data2]
-         * [소비 완료] data2 <- []
-         * [소비 시도] ? <- []
-         * [소비 완료] null <- []
+         * [소비 시도] ? <- [data1, data2]
+         * [소비 시도] ? <- [data1, data2]
          *
-         * 현재 상태 출력, 큐 데이터: []
+         * 현재 상태 출력, 큐 데이터: [data1, data2]
          * producer1: TERMINATED
          * producer2: TERMINATED
-         * producer3: TERMINATED
-         * consumer1: TERMINATED
-         * consumer2: TERMINATED
-         * consumer3: TERMINATED
+         * producer3: TIMED_WAITING
+         * consumer1: BLOCKED
+         * consumer2: BLOCKED
+         * consumer3: BLOCKED
          *
-         * == [생산자 먼저 실행] 종료, BoundedQueueV1 ==
+         * == [생산자 먼저 실행] 종료, BoundedQueueV2 ==
+         *
+         * [put] 큐가 가득 참, 생산자 대기
+         * [put] 큐가 가득 참, 생산자 대기
+         * [put] 큐가 가득 참, 생산자 대기
          */
 //        producerFirst(queue); // 생산자 먼저 실행
 
         /**
-         * == [소비자 먼저 실행] 시작, BoundedQueueV1 ==
+         * == [소비자 먼저 실행] 시작, BoundedQueueV2 ==
          *
          * 소비자 시작
          * [소비 시도] ? <- []
-         * [소비 완료] null <- []
+         * [take] 큐에 데이터가 없음, 소비자 대기
          * [소비 시도] ? <- []
-         * [소비 완료] null <- []
          * [소비 시도] ? <- []
-         * [소비 완료] null <- []
          *
          * 현재 상태 출력, 큐 데이터: []
-         * consumer1: TERMINATED
-         * consumer2: TERMINATED
-         * consumer3: TERMINATED
+         * consumer1: TIMED_WAITING
+         * consumer2: BLOCKED
+         * consumer3: BLOCKED
          *
          * 생산자 시작
          * [생산 시도] data1 -> []
-         * [생산 완료] data1 -> [data1]
-         * [생산 시도] data2 -> [data1]
-         * [생산 완료] data2 -> [data1, data2]
-         * [생산 시도] data3 -> [data1, data2]
-         * [put] 큐가 가득 참, 버림: data3
-         * [생산 완료] data3 -> [data1, data2]
+         * [생산 시도] data2 -> []
+         * [생산 시도] data3 -> []
          *
-         * 현재 상태 출력, 큐 데이터: [data1, data2]
-         * consumer1: TERMINATED
-         * consumer2: TERMINATED
-         * consumer3: TERMINATED
-         * producer1: TERMINATED
-         * producer2: TERMINATED
-         * producer3: TERMINATED
+         * 현재 상태 출력, 큐 데이터: []
+         * consumer1: TIMED_WAITING
+         * consumer2: BLOCKED
+         * consumer3: BLOCKED
+         * producer1: BLOCKED
+         * producer2: BLOCKED
+         * producer3: BLOCKED
          *
-         * == [소비자 먼저 실행] 종료, BoundedQueueV1 ==
+         * == [소비자 먼저 실행] 종료, BoundedQueueV2 ==
+         *
+         * [take] 큐에 데이터가 없음, 소비자 대기
+         * [take] 큐에 데이터가 없음, 소비자 대기
+         * [take] 큐에 데이터가 없음, 소비자 대기
          */
         consumerFirst(queue); // 소비자 먼저 실행
     }
