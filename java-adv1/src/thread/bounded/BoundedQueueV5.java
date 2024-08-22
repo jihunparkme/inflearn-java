af91d56851c39472ece7f5a0f95ff818572844c5
Jihun Park <jihunpark.tech@gmail.com>
2024-08-23 08:40:22 +0900

    [java adv1] 생산자 소비자 대기 공간 분리 준비

---
+++ java-adv1/src/thread/bounded/BoundedQueueV5.java
@@ -0,0 +1,76 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+
+public class BoundedQueueV5 implements BoundedQueue {
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition(); // ReentrantLock 스레드가 대기하는 공간 생성
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV5(int max) {
+        this.max = max;
+    }
+
+    public void put(String data) {
+        lock.lock();
+        try {
+            while (queue.size() == max) {
+                log("[put] 큐가 가득 참, 생산자 대기");
+                try {
+                    /**
+                     * Object.wait()와 유사한 기능
+                     * ReentrantLock에서 획득한 락을 반납하고 지정한 condition에 현재 스레드를 WAITING 상태로 보관
+                     */
+                    condition.await();
+                    log("[put] 생산자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            queue.offer(data);
+            log("[put] 생산자 데이터 저장, signal() 호출");
+            /**
+             * Object.notify()와 유사한 기능
+             * 지정한 condition에서 대기중인 스레드를 하나 깨움. 깨어난 스레드는 condition에서 탈출
+             */
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String take() {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                log("[take] 큐에 데이터가 없음, 소비자 대기");
+                try {
+                    condition.await();
+                    log("[take] 소비자 깨어남");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            String data = queue.poll();
+            log("[take] 소비자 데이터 획득, signal() 호출");
+            condition.signal();
+            return data;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
