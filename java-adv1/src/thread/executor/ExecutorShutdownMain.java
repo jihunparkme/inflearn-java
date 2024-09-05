1a08faedba4a209c7a75f60f79cacfc497b9d272
Jihun Park <jihunpark.tech@gmail.com>
2024-09-05 15:16:49 +0900

    [java adv1] feat: ExecutorService 우아한 종료 주석 추가

--- java-adv1/src/thread/executor/ExecutorShutdownMain.java
+++ java-adv1/src/thread/executor/ExecutorShutdownMain.java
@@ -9,6 +9,34 @@ import static util.MyLogger.log;
 
 public class ExecutorShutdownMain {
 
+    /**
+     * [pool-1-thread-2] taskB 시작
+     * [pool-1-thread-1] taskA 시작
+     * [     main] [pool=2, active=2, queuedTasks=2, completedTasks=0]
+     * [     main] == shutdown 시작 ==
+     * [     main] 서비스 정상 종료 시도
+     * [pool-1-thread-2] taskB 완료
+     * [pool-1-thread-2] taskC 시작
+     * [pool-1-thread-1] taskA 완료
+     * [pool-1-thread-1] longTask 시작
+     * [pool-1-thread-2] taskC 완료
+     * [     main] 서비스 정상 종료 실패 -> 강제 종료 시도
+     * [pool-1-thread-1] 인터럽트 발생, sleep interrupted
+     * [     main] == shutdown 완료 ==
+     * [     main] [pool=0, active=0, queuedTasks=0, completedTasks=4]
+     *
+     * Exception in thread "pool-1-thread-1" java.lang.RuntimeException: java.lang.InterruptedException: sleep interrupted
+     * 	at util.ThreadUtils.sleep(ThreadUtils.java:12)
+     * 	at thread.executor.RunnableTask.run(RunnableTask.java:23)
+     * 	at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1144)
+     * 	at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:642)
+     * 	at java.base/java.lang.Thread.run(Thread.java:1583)
+     * Caused by: java.lang.InterruptedException: sleep interrupted
+     * 	at java.base/java.lang.Thread.sleep0(Native Method)
+     * 	at java.base/java.lang.Thread.sleep(Thread.java:509)
+     * 	at util.ThreadUtils.sleep(ThreadUtils.java:9)
+     * 	...
+     */
     public static void main(String[] args) throws InterruptedException {
         ExecutorService es = Executors.newFixedThreadPool(2);
         es.execute(new RunnableTask("taskA"));
